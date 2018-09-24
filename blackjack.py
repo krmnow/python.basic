d3957ed1ca45a3008709fbdc18bfb7b692791829
krmnow <krzysiek.nowak@onet.eu>
2018-09-24 17:31:22 +0200

    add wins validation

--- blackjack.py
+++ blackjack.py
@@ -1,10 +1,9 @@
 import random
 
 try:
-    import Tkinter
+    import tkinter
 except ImportError:
     import Tkinter as tkinter
-mainWindow = tkinter.Tk()
 
 
 def load_images(card_images):
@@ -30,30 +29,70 @@ def load_images(card_images):
 
 def deal_card(frame):
     #pop next cart on top
-    next_card = deck.pop()
+    next_card = deck.pop(0)
     # add image
     tkinter.Label(frame, image=next_card[1], relief='raised').pack(side='left')
     return next_card
+
+def score_hand(hand):
+    score = 0
+    ace = False
+    for next_card in hand:
+        card_value = next_card[0]
+        if card_value == 1 and not ace:
+            ace = True
+            card_value = 11
+        score += card_value
+        if score > 21 and ace:
+            score -= 10
+            ace = False
+    return score
 
 def deal_dealer():
     deal_card(dealer_card_frame)
 
 def deal_player():
-    deal_card(player_card_frame)
+    player_hand.append(deal_card(player_card_frame))
+    player_score = score_hand(player_hand)
+    
+    player_score_label.set(player_score)
+    if player_score > 21:
+        result_text.set("Dealer wins")
+    
+    #    global player_score
+#    global player_ace
+ #   card_value = deal_card(player_card_frame)
+  #  if card_value == 1 and not player_ace:
+        
+   #     card_value = 11
+   # player_score += card_value
+    
+   # if player_score > 21 and player_ace:
+    #    player_score -= 18
+     #   player_ace = False
+   # player_score_label.set(player_score)
+   # if player_score > 21:
+   #     result_text.set("Dealer wins")
+    
+    print(locals())
 # Set up the screen
+
+mainWindow = tkinter.Tk()
 
 mainWindow.title("Black jack")
 mainWindow.geometry("640x480")
+mainWindow.configure(background="green")
+
 
 result_text = tkinter.StringVar()
 result = tkinter.Label(mainWindow, textvariable=result_text)
-result.grid(row=0, column=0, columnspace=3)
+result.grid(row=0, column=0, columnspan=3)
 card_frame = tkinter.Frame(mainWindow, relief="sunken", borderwidth=1, background="green")
 card_frame.grid(row=1, column=0, sticky="ew", columnspan=3, rowspan=2)
  
 dealer_score_label = tkinter.IntVar()
 tkinter.Label(card_frame, text="Dealer", background="green", fg="white").grid(row=0, column=0)
-tkinter.Label(card_frame, textvariable=dealer_score_label, backround="green", fg="white").grid(row=1, column=0)
+tkinter.Label(card_frame, textvariable=dealer_score_label, background="green", fg="white").grid(row=1, column=0)
 
 #embedded frame hold the card image
  
@@ -61,12 +100,15 @@ dealer_card_frame = tkinter.Frame(card_frame, background="green")
 dealer_card_frame.grid(row=0, column=1, sticky="ew", rowspan=2)
 
 player_score_label = tkinter.IntVar()
+
+
+
 tkinter.Label(card_frame, text="Player", background="green", fg="white").grid(row=2, column=0)
-tkinter.lable(card_frame, textvariable=player_score_label, backround="green", fg="white").grid(row=3, column=0)
+tkinter.Label(card_frame, textvariable=player_score_label, background="green", fg="white").grid(row=3, column=0)
 
 #embeded frame to holf the card images
 
-player_card_frame = tkinter.frame(card_frame, background="green")
+player_card_frame = tkinter.Frame(card_frame, background="green")
 player_card_frame.grid(row=2, column=1, sticky="ew", rowspan=2)
 
 button_frame = tkinter.Frame(mainWindow)

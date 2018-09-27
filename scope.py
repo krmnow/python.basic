78bb6d63b9fab4ab21e39d2a0bc3ea13f19174af
krmnow <krzysiek.nowak@onet.eu>
2018-09-27 10:53:35 +0200

    def fibonacci (faster than fib)

--- scope.py
+++ scope.py
@@ -19,5 +19,19 @@ def fib(n):
     else:
         return fib(n-1) + fib(n-2)
 
+def fibonacci(n):
+    if n == 0:
+        result = 0
+    elif n == 1:
+        result = 1
+    else:
+        n_minu1 = 1
+        n_minu2 = 0
+        for f in range(1, n + 1):
+            result = n_minu2 + n_minu1
+            n_minu2 = n_minu1
+            n_minu1 = result
+        return result
+
 for i in range(36):
-    print(i, factorial(i))
+    print(i, fibonacci(i))

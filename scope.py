9462e1510f842fad5b3bfe26eca655256fc71a53
krmnow <krzysiek.nowak@onet.eu>
2018-09-27 10:47:50 +0200

    function call itsefl

--- scope.py
+++ scope.py
@@ -1,4 +1,3 @@
-
 def fact(n):
     #calclulate n! literatively """"
     result = 1
@@ -7,5 +6,12 @@ def fact(n):
             result *= n
         return result
 
+def factorial(n):
+    # n! can also be defined as n *(n-1)
+    if n <= 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
 for i in range(130):
-    print(i, fact(i))
+    print(i, factorial(i))

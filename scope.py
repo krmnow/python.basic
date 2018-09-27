465a6813f84d4f9e3f7420eba63f2b8d874f58de
krmnow <krzysiek.nowak@onet.eu>
2018-09-27 10:44:01 +0200

    fibonnaci ex

--- scope.py
+++ scope.py
@@ -1,1 +1,11 @@
 
+def fact(n):
+    #calclulate n! literatively """"
+    result = 1
+    if n > 1:
+        for i in range(2, n + 1):
+            result *= n
+        return result
+
+for i in range(130):
+    print(i, fact(i))

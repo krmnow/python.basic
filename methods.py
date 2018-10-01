f83acd3e5f83988db8225c2620c110ecaa43285a
krmnow <krzysiek.nowak@onet.eu>
2018-10-01 15:27:31 +0200

    add tin example

--- methods.py
+++ methods.py
@@ -1,17 +1,25 @@
 class account:
     
     def __init__(self, name, balance):
-        self.name - name
+        self.name = name
         self.balance = balance
         print("Account cerated for " + self.name)
         
     def deposit(self, amount):
-        if amount >0:
+        if amount > 0:
             self.balance += amount
     def withdraw(self, amount):
-        if amount >0:
+        if amount > 0:
             self.balance -= amount
     def show_balance(self):
         print("Balance is {}".format(self.balance))
         
 
+if __name__ == '__main__':
+    tin = account("Tin", 0)
+    tin.show_balance()
+    
+    tin.deposit(1000)
+    tin.show_balance()
+    tin.withdraw(500)
+    tin.show_balance()

389cdaff40ac20290f4e88a367f87ee761b2baa2
krmnow <krzysiek.nowak@onet.eu>
2018-10-01 14:23:00 +0200

    namespace in class

--- objects.py
+++ objects.py
@@ -1,9 +1,14 @@
 class Kettl(object):
     
+    power_source = "electricy"
+    
     def __init__(self, make, price):
         self.make = make
         self.price = price
         self.on = False
+        
+    def switch_on(self):
+        self.on = True
         
 keywood = Kettl("Keywood", 8.99)
 print(keywood.make)
@@ -15,3 +20,21 @@ print(keywood.price)
 hamilton = Kettl("Hamilton", 14.55)
 
 print("Models: {} = {}, {} = {}".format(keywood.make, keywood.price, hamilton.make, hamilton.price))
+
+print("Models: {0.make} = {0.price}, {1.make} = {1.price}".format(keywood, hamilton))
+
+print(hamilton.on)
+hamilton.switch_on()
+print(hamilton.on)
+
+Kettl.switch_on(keywood)
+print(keywood.on)
+keywood.switch_on()
+
+print("*" * 20)
+keywood.power = 1.5
+#print(keywood.power)
+
+print(Kettl.power_source)
+print(keywood.power_source)
+print(hamilton.power_source)

2e4dfa4a1bf35f2f96484e63ae21cdbb71120697
krmnow <krzysiek.nowak@onet.eu>
2018-10-01 15:31:29 +0200

    case under 0

--- methods.py
+++ methods.py
@@ -8,9 +8,14 @@ class account:
     def deposit(self, amount):
         if amount > 0:
             self.balance += amount
+            self.show_balance()
+            
     def withdraw(self, amount):
-        if amount > 0:
+        if 0 < amount <= self.balance:
             self.balance -= amount
+        else:
+            print("Amount must be graeater than zero")
+        self.show_balance
     def show_balance(self):
         print("Balance is {}".format(self.balance))
         
@@ -19,7 +24,8 @@ if __name__ == '__main__':
     tin = account("Tin", 0)
     tin.show_balance()
     
-    tin.deposit(1000)
-    tin.show_balance()
-    tin.withdraw(500)
-    tin.show_balance()
+tin.deposit(1000)
+   # tin.show_balance()
+tin.withdraw(500)
+   # tin.show_balance()
+tin.withdraw(500)

bba734fba24fd036200caaf3dca7aa7b921dc922
krmnow <krzysiek.nowak@onet.eu>
2018-10-01 15:04:15 +0200

    create basic function

--- methods.py
+++ methods.py
@@ -1,1 +1,17 @@
+class account:
+    
+    def __init__(self, name, balance):
+        self.name - name
+        self.balance = balance
+        print("Account cerated for " + self.name)
+        
+    def deposit(self, amount):
+        if amount >0:
+            self.balance += amount
+    def withdraw(self, amount):
+        if amount >0:
+            self.balance -= amount
+    def show_balance(self):
+        print("Balance is {}".format(self.balance))
+        
 

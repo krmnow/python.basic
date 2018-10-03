5d49100af26c04ebbf6e0112961d4a8d504f8325
krmnow <krzysiek.nowak@onet.eu>
2018-10-03 10:18:22 +0200

    add show_transactions function

--- methods.py
+++ methods.py
@@ -1,14 +1,21 @@
+import datetime
+import pytz
+
+
+
 class account:
     
     def __init__(self, name, balance):
         self.name = name
         self.balance = balance
+        self.transaction_list = []
         print("Account cerated for " + self.name)
         
     def deposit(self, amount):
         if amount > 0:
             self.balance += amount
             self.show_balance()
+            self.transaction_list.append(pytz.utc.localize(datetime.datetime.utcnow(amount)))
             
     def withdraw(self, amount):
         if 0 < amount <= self.balance:
@@ -19,6 +26,14 @@ class account:
     def show_balance(self):
         print("Balance is {}".format(self.balance))
         
+    def show_transactions(self):
+        for date, amount in self.transaction_list:
+            if amount > 0:
+                tran_type = "deposited"
+            else:
+                tran_type = "withdrawn"
+                amount *= -1
+            print("{6} {} on {} (local time was) {}".format(amount, tran_type, date, date.astimezone()))
 
 if __name__ == '__main__':
     tin = account("Tin", 0)
@@ -29,3 +44,5 @@ tin.deposit(1000)
 tin.withdraw(500)
    # tin.show_balance()
 tin.withdraw(500)
+
+tim.show_transactions()

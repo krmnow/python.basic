2a0b41489678c0cc4ee3f30b884d9bf60e8f1972
krmnow <krzysiek.nowak@onet.eu>
2018-10-04 17:39:17 +0200

    add class album

--- docstrings.py
+++ docstrings.py
@@ -5,3 +5,14 @@ class song:
         self.artist = artist
         self.duration = duration
 
+class album:
+    
+    def __init__(self, name, year, artist=None):
+        self.name = name
+        self.year = year
+        if artist is None:
+            self.artist = Artist("Various artist")
+        else:
+            self.artist = artist
+            
+        self.tracks = []

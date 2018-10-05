51b3e01e4ec78faef4f3d881a15008ada2d80667
krmnow <krzysiek.nowak@onet.eu>
2018-10-05 08:27:16 +0200

    load data from external file

--- docstrings.py
+++ docstrings.py
@@ -33,3 +33,12 @@ class artist:
     def add_album(self, album):
         
         self.albums.append(album)
+        
+def load_data():
+    new_artist = None
+    new_album = None
+    artist_list = []
+    
+    with open("album.txt", "r") as albums:
+        for line in album:
+            artist_field, album_field, year_field, song_field = tuple(line.strip('\n').split('\t')) 

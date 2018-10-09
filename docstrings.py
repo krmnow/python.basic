de531ab45f018ebff76644e2a23dced308480516
krmnow <krzysiek.nowak@onet.eu>
2018-10-09 11:47:49 +0200

    fix some bugs

--- docstrings.py
+++ docstrings.py
@@ -1,7 +1,7 @@
 class song:
     
     def __init__(self, title, artist, duration=0):
-        self.title = title
+        self.name = title
         self.artist = artist
         self.duration = duration
 
@@ -19,10 +19,13 @@ class album:
         
     def add_song(self, song, position=None):
 
-        if position is None:
-            self.tracks.append(song)
-        else:
-            self.tracks.insert(position, song)
+        
+        song_found = find_object(song, self.tracks)
+       if song_found is None: 
+            if position is None:
+                self.tracks.append(song)
+            else:
+                self.tracks.insert(position, song)
             
 class artist:
     
@@ -75,7 +78,7 @@ def create_checkfile(artist_list):
         for new_artist in artist_list:
             for new_album in new_artist.albums:
                 for new_song in new_album.tracks:
-                    print("{0.name}\t{1.name}\t{1.year}\t{2.title}".format(new_artist, new_album, new_song),
+                    print("{0.name}\t{1.name}\t{1.year}\t{2.name}".format(new_artist, new_album, new_song),
                           file=checkfile)
         
 if __name__ ** '__main__':

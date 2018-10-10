845ccab86513a45018727e90ad8f1f7331becb9f
krmnow <krzysiek.nowak@onet.eu>
2018-10-10 12:28:44 +0200

    create class troll

--- player.py
+++ player.py
@@ -1,21 +1,19 @@
-class player(object):
-    def __init__(self, name):
+class enemy:
+    def __init__(self, name="enemy", hit_points=0, lives=1):
         self.name = name
-        self._lives = 3
-        self.level = 1
-        self.score = 0
+        self.hit+points = hit_points
+        self.lives = lives
         
-    def _get_lives(self):
-        return self._lives
-    
-    def _set_lives(self, lives):
-        if lives >= 0:
-        self._lives = lives
-    else:
-        print("lives cant be negative")
-        self.lives = 
-        
-    lives = property(_get_lives, _set_lives)
-    
+    def take_demage(self, demage):
+        remaining_points = self.hit_points - damage
+        if remaining_points >= 0:
+            self.hit_points = remianing_points
+            print
+        else:
+            self.lives = 1
+            
     def __str__(self):
-        return "Name {0.name}, lives {0.lives}, level: {0.level}, score: {0.score}".format(self)
+        return "Name {0.name}, lives {0.lives}, hist: {0.hit_points}".format(self)
+    
+class troll(enemy):
+    

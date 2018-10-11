f4c803100d687987fc0fcc8bacb71b0326b9dc76
krmnow <krzysiek.nowak@onet.eu>
2018-10-11 12:42:38 +0200

    add pingiwm class

--- duck.py
+++ duck.py
@@ -7,3 +7,24 @@ class duck(object):
         
     def quack(self):
         print("kwa kwa")
+        
+        
+class pingwin(self):
+
+    def walk(self):
+        print("Chce chodzic")
+
+    def swim(self):
+        print("Nie umiem plywac")
+
+    def quack(self):
+        print("Jestem pinginem, odzywam sie inaczej)"
+
+def test_duck(duck):
+    duck.walk()
+    duck.swim()
+    duck.quack()
+    
+if __ name_ == '__main__':
+    dondald = duck()
+    test_duck(donald)

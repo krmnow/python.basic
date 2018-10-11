aac31e070ecd4bda5da3fdee4c06cec2fa80572d
krmnow <krzysiek.nowak@onet.eu>
2018-10-11 12:37:54 +0200

    simple class duck

--- duck.py
+++ duck.py
@@ -1,3 +1,9 @@
 class duck(object):
     
-    
+    def walk(self):
+        print("waddle, waddle, waddle")
+    def swim(self):
+        print("Wskakuj do wody")
+        
+    def quack(self):
+        print("kwa kwa")

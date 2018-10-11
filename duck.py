e43be05badaee8ae8037d8df4ae2aadb00f76f56
krmnow <krzysiek.nowak@onet.eu>
2018-10-11 16:10:13 +0200

    fly case

--- duck.py
+++ duck.py
@@ -1,3 +1,16 @@
+class wing(object):
+    
+    def __init__(self, ratio):
+            self.ratio = ratio
+    
+    def fly(self):
+        if self.ratio > 1:
+            print("Smiesznie")
+        elif:
+            print("ciezko idzie ale latam")
+        else:
+            print("dziekuje, postoje")
+
 class duck(object):
     
     def walk(self):
@@ -20,11 +33,11 @@ class pingwin(self):
     def quack(self):
         print("Jestem pinginem, odzywam sie inaczej)"
 
-def test_duck(duck):
-    duck.walk()
-    duck.swim()
-    duck.quack()
+#def test_duck(duck):
+ #   duck.walk()
+  #  duck.swim()
+   # duck.quack()
     
 if __ name_ == '__main__':
     dondald = duck()
-    test_duck(donald)
+  #  test_duck(donald)

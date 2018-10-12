5e0922540c7432317a5e251b68be212cdaa95414
krmnow <krzysiek.nowak@onet.eu>
2018-10-12 12:30:25 +0200

    open into file

--- composition.py
+++ composition.py
@@ -8,8 +8,8 @@ class tag(object):
     def __str__(self):
         return "{0.start_tag}{0.contecs}{0.end_tag}".format(self)
         
-    def display(self):
-        print(self)
+    def display(self, file=None):
+        print(self, file=file)
 
 class doctype(tag):
     def __init__(self):
@@ -31,11 +31,11 @@ class body(tag):
         new_tag = tag (name, contects)
         self.body_contects.append(new_tag)
         
-    def display(self):
+    def display(self, file=None):
         for tag in self._body_contects:
             self.contects += str(tag)
             
-            super().display
+            super().display(file=file)
             
 class html_doc(object):
     
@@ -47,12 +47,12 @@ class html_doc(object):
     def add_tag(self, name, contects):
         self._body.add_tag(name, contects)
         
-    def display(self):
-        self._doc_type.display()
-        print('<html>')
-        self._head.display()
-        self._body.display()
-        print('</html>')
+    def display(self, file=None):
+        self._doc_type.display(file=file)
+        print('<html>', file=file)
+        self._head.display(file=file)
+        self._body.display(file=file)
+        print('</html>', file=file)
 
 if __name__ == '__main__':
 my_page = html_doc()

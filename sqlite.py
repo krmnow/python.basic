7bea0e5a7e32f6376454bc6b72b0ca25b7b0f2f6
krmnow <krzysiek.nowak@onet.eu>
2018-10-16 14:44:46 +0200

    run sample db

--- sqlite.py
+++ sqlite.py
@@ -3,4 +3,8 @@ import sqlite3
 db = sqlite3.connect("contacts.sqlite")
 
 db.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)")
-db.execute("INSERT INTO concats(name, phone, email) VALUES ('Tim', 43354252, 'email@email.pl')")
+db.execute("INSERT INTO contacts(name, phone, email) VALUES ('Tim', 43354252, 'email@email.pl')")
+db.execute("INSERT INTO contacts VALUES ('Ida', 5435, 'myemail@gmail.pl')")
+
+db.close()
+

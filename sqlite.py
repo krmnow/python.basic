5b9db1c8fe3ed595dcbeed74f1efa25ba1bdac6d
krmnow <krzysiek.nowak@onet.eu>
2018-10-16 14:42:23 +0200

    insert first user

--- sqlite.py
+++ sqlite.py
@@ -3,3 +3,4 @@ import sqlite3
 db = sqlite3.connect("contacts.sqlite")
 
 db.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)")
+db.execute("INSERT INTO concats(name, phone, email) VALUES ('Tim', 43354252, 'email@email.pl')")

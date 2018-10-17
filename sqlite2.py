166abcc80e104806dede82f41fa76ad44d5c641f
krmnow <krzysiek.nowak@onet.eu>
2018-10-17 10:35:08 +0200

    add phone number validation

--- sqlite2.py
+++ sqlite2.py
@@ -5,7 +5,11 @@ db = sqlite3.connect("contacs.sqlite")
 new_email = 'anotheremail@email.pl'
 phone = input("Please enter phone number")
 
-update_sql = "UPDATE contacts SET email = '{}' where phone = {}".format(new_email, phone)
+#update_sql = "UPDATE contacts SET email = '{}' where phone = {}".format(new_email, phone)
+update_sql = "UPDATE contacts SET email = ? WHERE phone = ?"
+print(update_sql)
+
+
 udpate_cursor = db.cursor()
 update_cursor.execute(update_sql)
 print("{} row update".format(update_cursor.connection == db))

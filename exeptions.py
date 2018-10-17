8aaba1ad6ebc595d6c4a9dacaf8b995338fdf90b
krmnow <krzysiek.nowak@onet.eu>
2018-10-17 13:49:59 +0200

    create function with RecursionError exceprion

--- exeptions.py
+++ exeptions.py
@@ -1,1 +1,13 @@
-
+def factorial(n):
+    # n! can also be defined as n = (n-1)
+    if n <= 1:
+        return 1
+    else:
+     return n * factorial(n-1)
+ 
+try:    
+    print (factorial(10))
+except RecursionError:
+    print("This progranm calculate factorial that large")
+    
+print("program terminating")

ea49785c618aa975292d3dfcda615585648c7d3c
krmnow <krzysiek.nowak@onet.eu>
2018-10-19 15:22:50 +0200

    add database

--- rolling_back.py
+++ rolling_back.py
@@ -1,5 +1,26 @@
+import sqlite3
+
+db = sqlite3.connect('account.sqlite')
+db.execute("CREATE TABLE IF NOT EXISTS accounts (name TEXT PRIMARY KEY NOT NULL, balance INTEGER NOT NULL")
+db.execute("CREATE TABLE IF NOT EXISTS transactions (time TIMESTAMP NOT NULL,"
+                                                     " account TEXT NOT NULL amount INTEGER NOT NULL, PRIMARY KEY (time, account))")
+
+
 class accout(object):
     def __init__(self, name: str, opening_balance: float = 0.0):
+        cursor = db.execute("SELECT name, balance FRON accoubts WHERE (name = ?", (name),)
+        row = cursor.fetchone()
+        
+        if row:
+            self.name, self._balance = row
+            print("Retrived record for {}".format(self.name), end="")
+        else:
+            self.name = name
+            self._balance = opening_balance
+            cursor.execute("INSERT INTO accounts VALUES(?, ?)", (name, opening_balance))
+            cursor.connection.commit()
+        self.show_balance()
+            
         self.name = name
         self._balance = opening_balance
         print("accournt created for {}".format(self.name), end="")
@@ -30,4 +51,3 @@ if __name__ == '__main__':
     john.withdraw(0.30)
     john.withdraw(0)
     john.show_balance()
-    

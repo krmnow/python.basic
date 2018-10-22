3f6f64de9578a7ab0de56bc711a6fa807d7b0476
krmnow <krzysiek.nowak@onet.eu>
2018-10-22 13:42:37 +0200

    Update rolling_back.py

--- rolling_back.py
+++ rolling_back.py
@@ -3,7 +3,7 @@ import sqlite3
 db = sqlite3.connect('account.sqlite')
 db.execute("CREATE TABLE IF NOT EXISTS accounts (name TEXT PRIMARY KEY NOT NULL, balance INTEGER NOT NULL)")
 db.execute("CREATE TABLE IF NOT EXISTS transactions (time TIMESTAMP NOT NULL,"
-                                                     " account TEXT NOT NULL amount INTEGER NOT NULL, PRIMARY KEY (time, account))")
+           " account TEXT NOT NULL amount INTEGER NOT NULL, PRIMARY KEY (time, account))")
 
 
 class accout(object):

69d625f275d490e6ca7bd4ad796f023cfef1c086
krmnow <krzysiek.nowak@onet.eu>
2018-10-23 11:24:05 +0200

    create database_browser.py

---
+++ database_browser.py
@@ -0,0 +1,6 @@
+import sqlite
+
+conn = sqlite3.connect('music.sqlite')
+
+for x in conn.execute("SELECT albums.name FROM albums WHERE albums.artist = ? ORDER BY albums.name", (196,))!=:
+    print(x)

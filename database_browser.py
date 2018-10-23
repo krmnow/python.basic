f7c99197a26c701ba17ffb9e56c1a596a942c51b
krmnow <krzysiek.nowak@onet.eu>
2018-10-23 11:32:35 +0200

    add mainwidows

--- database_browser.py
+++ database_browser.py
@@ -6,3 +6,17 @@ except ImportError:
     import Tkinter as tkinter
     
 conn = sqlite3.connect('music.sqlite')
+
+mainwindow = tkinter.Tk()
+mainwindow.title('Music BD Browser')
+mainwindow.geometry('1824x763')
+
+mainwindow.columnconfigure(0, weight=2)
+mainwindow.columnconfigure(1, weight=2)
+mainwindow.columnconfigure(2, weight=2)
+mainwindow.columnconfigure(3, weight=1)
+
+mainwindow.rowconfigure(0, weight=1)
+mainwindow.rowconfigure(1, weight=5)
+mainwindow.rowconfigure(2, weight=5)
+mainwindow.rowconfigure(3, weight=1)

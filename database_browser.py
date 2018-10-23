c381673a67e7a28c79554d59329edbb21daa0cb8
krmnow <krzysiek.nowak@onet.eu>
2018-10-23 11:28:51 +0200

    Update database_browser.py

--- database_browser.py
+++ database_browser.py
@@ -1,6 +1,8 @@
-import sqlite
+import sqlite3
 
+try:
+    import tkinter
+except ImportError:
+    import Tkinter as tkinter
+    
 conn = sqlite3.connect('music.sqlite')
-
-for x in conn.execute("SELECT albums.name FROM albums WHERE albums.artist = ? ORDER BY albums.name", (196,)):
-    print(x)

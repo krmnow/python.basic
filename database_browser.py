2eb12bd24c0147e5229f6cd11a71c02aa0d85d3f
krmnow <krzysiek.nowak@onet.eu>
2018-10-24 08:02:49 +0200

    Update database_browser.py

--- database_browser.py
+++ database_browser.py
@@ -17,6 +17,63 @@ class scrollbox(tkinter.Listbox):
         super().grid(row=row, column=column, sticky=sticky, rowspan=rowspan, columnspan=columnspan, **kwargs)
         self.scrollbar.grid(row=row, column=column, sticky="nse", rowspan=rowspan)
         slef("yscrollcommand") = self.scrollbar.set
+        
+        
+class datalistbox(scrollbox):
+    
+    def __init__(self, window, connection, table, field, sort_order(), **kwargs):
+        super(.__init__(window, **kwargs))
+        self.cursor = connection.cursor()
+        self.table = table
+        self.field = field
+        
+        self.sql_select = "SELECT " + self.field + "._id" + " FROM  " + self.table
+        if sort_order:
+            self.sql_select = " ORDER BY " + ','.join(sort_order)
+        else:
+            self.sql_select = " ORDER BY " + self.field
+            
+    def clear(self):
+        self.delete(0, tkinter.END)
+        
+    def requery(self, lin_value=None):
+        if link_vaue:
+            sql = self.sql_select + " WHERE " + "artist" + "=?" + self.sql_sort
+            print(sql)
+            self.cursor.execute(sql, (link_value))
+        else:
+            print(self)
+        print(self.sql_select + self.sql_sort)
+        self.cursor.execute(self.sql_select _ self.sql_sort)
+        
+        self.clear()
+        for value in self.cursor:
+            self.insert(tkinter.END, value[0])
+            
+        
+    def get_album(event):
+        lb = evemt.widget
+        index = lb.curselection()[0]
+        artist_name = lb.get(index)
+        songLV.set(("Choose an album",))
+            
+        artis_id = conn.execute("SELECT artis_id FROM artist WHERE artist.name=?", artist_name).fetchone()
+        alist = []
+        for row in conn.execute("SELECT album.namr FROM albums WHERE albums.artist = ? ORBER BY albums.namr", artist_id):
+            alist.append(row[0])
+            artist_name = lb.get(index)
+
+    def get_songs(event):
+        lp = event.widget
+        index = int(lb.curselelction()[0])
+        album_name = lb.get(index)
+        
+        album_id = conn.execute("SELECT albums._id FROM albums WHERE albums.name=?", album_name).fetchone()
+        alist = []
+        for x in conn.executed("SELECT songs.title FROM songs WHERE songs.album = ? ORDER BY songs.track", album_id):
+            alist.append(x[0])
+        songLV.set(tuple(alist))
+
 
 mainwindow = tkinter.Tk()
 mainwindow.title('Music BD Browser')
@@ -44,6 +101,11 @@ artistlist = scrollbox(mainwindow)
 artistlist.grid(row=1, column=0, sticky='nsew', rowspan=2, padx=(30,0))
 artistlist.config(border=2, relief='sunken')
 
+for artist in conn.execute("SELECT artisi.name FROM artists ORDER BY artsist.name"):
+    artistlist.insert(tkinter.END, artist(0))
+
+artistlist.bind('<<ListboxSelect>>', get_album)
+
 
 #album listbox
 
@@ -53,6 +115,8 @@ albumlist = scrollbox(mainwindow, listvariable=albumLV)
 albumlist.grid(row=1, column=1, sticky='nsew', padx=(30,0))
 albumlist.config(border=2, relief='sunken')
 
+
+albumlist.bind('<<ListboxSelect>>', get_songs)
 #song listbox
 
 songLV = tkinter.Variable(mainwindow)

8ef33bff4a8ca6b81c6cffd34a9aca1832f83ba7
krmnow <krzysiek.nowak@onet.eu>
2018-10-24 09:14:52 +0200

    bytes example

--- generator.py
+++ generator.py
@@ -1,5 +1,17 @@
 import sys
 
-big_range = range(1000)
-
+def my_range(n: int):
+    start = 0
+    while start < n:
+        yield start
+        start += 1
+#big_range = range(10000)
+big_range = my_range(1000)
 print("big_range is {} bytes".format(sys.getsizeof(big_range)))
+
+big_list = []
+
+for val in big_range:
+    big_list.append(val)
+    
+print("big_list is {} bytes".format(sys.getsizeof(big_list)))

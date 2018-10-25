cd0bdaebff080b72d1fdb23f8297058a9be97bba
krmnow <krzysiek.nowak@onet.eu>
2018-10-25 09:29:31 +0200

    Update conditional_comprehensions.py

--- conditional_comprehensions.py
+++ conditional_comprehensions.py
@@ -10,7 +10,9 @@ meals = []
 for meal in menu:
     if "span" not in meal:
         meals.append(meal)
+    else:
+         meals.append("a neal was skipped")
 print(meals)
 
-meals = (meal for meal in menu if "span" in meal):
-    print(meals)
+meals = [meal for meal in menu if "spam" not in meal]
+print(meals)

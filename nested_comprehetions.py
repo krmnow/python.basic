89dbce2f2e64c23b9b0bcb80d19f3f92319716aa
krmnow <krzysiek.nowak@onet.eu>
2018-10-26 09:13:09 +0200

    Update nested_comprehetions.py

--- nested_comprehetions.py
+++ nested_comprehetions.py
@@ -10,3 +10,10 @@ for burger in burgers:
         
 for meals in [{burger, toping} for burger in burgers for toping in topings]:
     print(meals)
+    
+for i in range(1, 11):
+    for j in range(1, 11):
+        print(i, i * j)
+
+times = ({i, i * j} for i in range(1, 11) for j in range(1, 11))
+print(times)    

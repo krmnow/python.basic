4c5eeb308b3fe617858f3c931cab82630f8471f3
krmnow <krzysiek.nowak@onet.eu>
2018-10-26 07:46:06 +0200

    Create nested_comprehetions.py

---
+++ nested_comprehetions.py
@@ -0,0 +1,9 @@
+burgers = ["beef", "chicken", "spicy bean"]
+topings = ["cheese", "eggs", "beans", "span"]
+
+meals = [{burger, toping} for burger in burgers for toping in topings]
+print(meals)
+
+for burger in burgers:
+    for toping in topings:
+        print((burger, toping))

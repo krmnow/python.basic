9db75a86abcaedbdb258f17b2ffe0c114c6d722c
krmnow <krzysiek.nowak@onet.eu>
2018-10-26 12:21:01 +0200

    build caps functions

--- map.py
+++ map.py
@@ -1,8 +1,21 @@
 text = "what is the best team in the world"
 
 
-map_capitals = list(map(str.upper, text))
-print(map_capitals)
+def corp_caps():
+    capitals = [char.upper() for char in text]
+    return capitals
+    
+def map_caps():
+    map_capitals = list(map(str.upper, text))
+    return map_capitals
+
 
 map_words = list(map(str.upper, text.split(' ')))
 print(map_words)
+
+for x in map(str.upper, text.split(' ' )):
+    print(x)
+    
+if __name__ == '__main__':
+    print(corp_caps())
+    print(map_caps())

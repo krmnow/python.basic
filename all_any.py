68dd4b2180403df264ad92a78914451a11e8214a
krmnow <krzysiek.nowak@onet.eu>
2018-10-29 10:33:52 +0100

    Update all_any.py

--- all_any.py
+++ all_any.py
@@ -1,6 +1,8 @@
 from data import people, basic_plants_list, plants_list
 
-if all([person[1] for person in people]):
+people = []
+
+if  bool(people) and all([person[1] for person in people]):
     print("sending email")
 else:
     print("user must edit the list of reciptiens")

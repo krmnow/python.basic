dff9ec568034cb009ce896cb3b6de55a63b79573
krmnow <krzysiek.nowak@onet.eu>
2018-10-29 10:38:28 +0100

    Update all_any.py

--- all_any.py
+++ all_any.py
@@ -11,3 +11,8 @@ if any([plant.plant_type == "Grass" for plant in plants_list]):
     print("This pack contains grass")
 else:
 print("No grasses in this plack")    
+
+if any(plant.plant_type == "Grass" for plant in plans_dict.values()):
+    print("The dict contain grasess")
+else:
+    print("No grasses in the dict")

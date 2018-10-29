5e6618fdae77ec39f7f087f0abe6d40c8f7b9c72
krmnow <krzysiek.nowak@onet.eu>
2018-10-29 10:36:10 +0100

    Update all_any.py

--- all_any.py
+++ all_any.py
@@ -6,3 +6,8 @@ if  bool(people) and all([person[1] for person in people]):
     print("sending email")
 else:
     print("user must edit the list of reciptiens")
+
+if any([plant.plant_type == "Grass" for plant in plants_list]):
+    print("This pack contains grass")
+else:
+print("No grasses in this plack")    

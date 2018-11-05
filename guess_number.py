4eae1d1157a1bf2192283dd50c5d096c53e1724c
krmnow <krzysiek.nowak@onet.eu>
2018-11-05 13:37:05 +0100

    ex 7 completed

--- guess_number.py
+++ guess_number.py
@@ -7,5 +7,15 @@ count = 0
 while guess != number and guess != "exit":
     guess = input("What's your guess?")
     if guess == "exit":
-
         break
+    
+    guess = int(guess)
+    count += 1
+    
+    if guess < number:
+        print("Too low")
+    elif guess > number:
+        print("Too high")
+    else:
+        print("You go it!")
+        print("And it only took you {} tries!".format(count))

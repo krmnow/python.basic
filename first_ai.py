b7572a1c3e365074ca0495f6718fc25e0fe29bc8
krmnow <krzysiek.nowak@onet.eu>
2018-11-06 14:18:28 +0100

    Update first_ai.py

--- first_ai.py
+++ first_ai.py
@@ -3,6 +3,9 @@ import numpy as np
 def sigoid(x):
     return 1 / (1 + np.exp(-x))
 
+def sigoid_deri(x):
+    return x *(1 - x)
+
 training_inputs = np.array([[0,0,1],
                             [1,1,1],
                             [1,0,1],
@@ -23,5 +26,13 @@ for i in range(1):
     
     outputs = sigoid(np.dot(input_layer, synaptic_weights))
     
+    error =  training_outputs - outputs
+     
+    adjustmets = error * sigoid_deri(outputs)
+    
+    synaptic_weights += np.dot(input_layer.T, adjustmets)
+    
+print("Synaptic weights after training: ")
+print(synaptic_weights )
 print("Outputs after: ")
 print(outputs)

b5a5f57581696b2a6ed52fe8dc5b93581daf437c
krmnow <krzysiek.nowak@onet.eu>
2018-11-07 10:37:03 +0100

    spłaszczenie pików

--- second_ai.py
+++ second_ai.py
@@ -15,3 +15,15 @@ print(X_train.shape, X_test.shape)
 
 ((60000, 28, 28), (10000, 28, 28))
 
+#splaszczenie
+
+if len(X_train.shape) == 3:
+    num_pixels = X_train.shape[1] * X_train.shape[2]
+    X_train = X_train.reshape(X_trainshape[0], num_pixels).astype("float32")
+    X_test = X_test.reshape(X_test,shape[0], num_pixels).astype("float32")
+
+if np.max(X_train) == 255: X_train /= 255
+if np.max(X_test) == 255: X_test /= 255
+
+num_pixels = X_train.shape[1]
+num_classes = y.train.shape[1] 

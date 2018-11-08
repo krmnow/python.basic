496205821bbd63a97694b5c1f746c75855761c76
krmnow <krzysiek.nowak@onet.eu>
2018-11-08 13:38:40 +0100

    Update fourt_ai.py

--- fourt_ai.py
+++ fourt_ai.py
@@ -1,50 +1,74 @@
-import numpy as numpy
+from numpy import exp, array, random, dot
 
-def nonlin(x, deriv=False):
-    if deriv == True:
-        return x*(1-x)
-    return 1/(1+np,exp(-x))
-    
-#inout data
-x = np.array([[0,0,1]],
-             [0,1,1],
-             [1,0,1],
-             [1,1,1])
 
-y = np.array([[0],
-              [1],
-              [1],
-              [0]])
-    
-np.random.seed(1)
+class NeuralNetwork():
+    def __init__(self):
+        # Seed the random number generator, so it generates the same numbers
+        # every time the program runs.
+        random.seed(1)
 
-#synapses
+        # We model a single neuron, with 3 input connections and 1 output connection.
+        # We assign random weights to a 3 x 1 matrix, with values in the range -1 to 1
+        # and mean 0.
+        self.synaptic_weights = 2 * random.random((3, 1)) - 1
 
-syn0 = 2*np.random.random((3,4)) - 1
-syn1 = 2*np.random.random((4,1)) - 1
+    # The Sigmoid function, which describes an S shaped curve.
+    # We pass the weighted sum of the inputs through this function to
+    # normalise them between 0 and 1.
+    def __sigmoid(self, x):
+        return 1 / (1 + exp(-x))
 
-#training step
+    # The derivative of the Sigmoid function.
+    # This is the gradient of the Sigmoid curve.
+    # It indicates how confident we are about the existing weight.
+    def __sigmoid_derivative(self, x):
+        return x * (1 - x)
 
-for j in range(6000):
-    l0 = X
-    l1 = nonlin(np.dot(l0, syn0))
-    l2 = nonlin(np.dot(l1, syn1))
-    
-    l2_error = y - l2
-    
-    if j % 10000 == 0:
-        print("Error: " + str(np.mean(np.abs(l2_error)))
-    
-        l2_delta = l2_error * nonlin(l2, deriv=True)
-    
-        l1_error = l2_delta.dot(syn1.T)
-    
-        l1_delta = l1_error * nonlin(l1,deriv=True)
-    
-    #updatwe weights
-    
-    syn1 = l1.T.dot(l2_delta)
-    syn0 += l0.T.dot(l1_delta)
-    
-print("Output after training")
-print(l2)
+    # We train the neural network through a process of trial and error.
+    # Adjusting the synaptic weights each time.
+    def train(self, training_set_inputs, training_set_outputs, number_of_training_iterations):
+        for iteration in range(number_of_training_iterations):
+            # Pass the training set through our neural network (a single neuron).
+            output = self.think(training_set_inputs)
+
+            # Calculate the error (The difference between the desired output
+            # and the predicted output).
+            error = training_set_outputs - output
+
+            # Multiply the error by the input and again by the gradient of the Sigmoid curve.
+            # This means less confident weights are adjusted more.
+            # This means inputs, which are zero, do not cause changes to the weights.
+            adjustment = dot(training_set_inputs.T, error * self.__sigmoid_derivative(output))
+
+            # Adjust the weights.
+            self.synaptic_weights += adjustment
+
+    # The neural network thinks.
+    def think(self, inputs):
+        # Pass inputs through our neural network (our single neuron).
+        return self.__sigmoid(dot(inputs, self.synaptic_weights))
+
+
+if __name__ == "__main__":
+
+    #Intialise a single neuron neural network.
+    neural_network = NeuralNetwork()
+
+    print("Random starting synaptic weights: ")
+    print (neural_network.synaptic_weights)
+
+    # The training set. We have 4 examples, each consisting of 3 input values
+    # and 1 output value.
+    training_set_inputs = array([[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1]])
+    training_set_outputs = array([[0, 1, 1, 0]]).T
+
+    # Train the neural network using a training set.
+    # Do it 10,000 times and make small adjustments each time.
+    neural_network.train(training_set_inputs, training_set_outputs, 10000)
+
+    print("New synaptic weights after training: ")
+    print(neural_network.synaptic_weights)
+
+    # Test the neural network with a new situation.
+    print("Considering new situation [1, 0, 0] -> ?: ")
+    print(neural_network.think(array([1, 0, 0])))

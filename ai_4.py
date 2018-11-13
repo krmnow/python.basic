76f04518e65d48ca1353aa7edffcfaa8f92a6b86
krmnow <krzysiek.nowak@onet.eu>
2018-11-13 15:23:22 +0100

    Update ai_4.py

--- ai_4.py
+++ ai_4.py
@@ -1,1 +1,9 @@
+import numpy as np
 
+# X = (hours sleeping, hours studying), y = score on test
+X = np.array(([2, 9], [1, 5], [3, 6]), dtype=float)
+y = np.array(([92], [86], [89]), dtype=float)
+
+# scale units
+X = X/np.amax(X, axis=0) # maximum of X array
+y = y/100 # max test score is 100

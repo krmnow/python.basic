8f18ca9df222a29c7d87c9ad13027ea9ec4bf77d
krmnow <krzysiek.nowak@onet.eu>
2018-11-13 15:24:46 +0100

    Update ai_4.py

--- ai_4.py
+++ ai_4.py
@@ -1,10 +1,4 @@
-
-class Neural_Network(object):
-  def __init__(self):
-    #parameters
-    self.inputSize = 2
-    self.outputSize = 1
-    self.hiddenSize = 3import numpy as np
+import numpy as np
 
 # X = (hours sleeping, hours studying), y = score on test
 X = np.array(([2, 9], [1, 5], [3, 6]), dtype=float)
@@ -13,3 +7,10 @@ y = np.array(([92], [86], [89]), dtype=float)
 # scale units
 X = X/np.amax(X, axis=0) # maximum of X array
 y = y/100 # max test score is 100
+
+class Neural_Network(object):
+  def __init__(self):
+    #parameters
+    self.inputSize = 2
+    self.outputSize = 1
+    self.hiddenSize = 3

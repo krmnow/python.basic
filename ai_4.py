ee8ebc81bb1e91a77df6dd82558058644acb823b
krmnow <krzysiek.nowak@onet.eu>
2018-11-14 20:41:03 +0100

    Update ai_4.py

--- ai_4.py
+++ ai_4.py
@@ -30,3 +30,11 @@ S(1.79832) = .8579443067
 #weights
 self.W1 = np.random.randn(self.inputSize, self.hiddenSize) # (3x2) weight matrix from input to hidden layer
 self.W2 = np.random.randn(self.hiddenSize, self.outputSize) # (3x1) weight matrix from hidden to output layer
+
+def forward(self, X):
+#forward propagation through our network
+  self.z = np.dot(X, self.W1) # dot product of X (input) and first set of 3x2 weights
+  self.z2 = self.sigmoid(self.z) # activation function
+  self.z3 = np.dot(self.z2, self.W2) # dot product of hidden layer (z2) and second set of 3x1 weights
+  o = self.sigmoid(self.z3) # final activation function
+  return o

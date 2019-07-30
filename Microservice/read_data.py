3e3375e81038fd47a8308c3a8415af02d806222b
krmnow <krzysiek.nowak@onet.eu>
2019-07-30 13:48:17 +0200

    Update read_data.py

--- Microservice/read_data.py
+++ Microservice/read_data.py
@@ -1,9 +1,23 @@
-from nameko.rpc import rpc
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
 
 
-class GreetingService:
-    name = "greeting_service"
+def make_soup(url):
+	html = urlopen(url).read()
+	return BeautifulSoup(html)
 
-    @rpc
-    def hello(self, name):
-        return "Hello, {}!".format(name)
+def get_images(url):
+	soup = make_soup(url)
+
+	images = [img for img in soup.findAll('img')]
+	print (str(len(images)) + "images found.")
+	print 'Downloading images to current working directory.'
+	#compile our unicode list of image links
+	image_links = [each.get('src') for each in images]
+	for each in image_links:
+		filename=each.split('/')[-1]
+		urllib.urlretrieve(each, filename)
+	return image_links
+
+get_images('https://www.lfc.pl')
